0d745d0b1b2079abaf3a80d7faf437a758815992
Felix Nana Yaw Yeboah-Jefferson <32690370+jaeyholic@users.noreply.github.com>
2018-11-22 18:18:16 +0000

    menu

--- src/components/Menu/index.vue
+++ src/components/Menu/index.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="container menu_container">
-    <div class="top">
+    <div class="top" v-scroll-reveal.reset>
       <h2>Menu</h2>
     </div>
-    <div class="buttons">
-      <md-button v-for="(button, index) in buttons" 
+    <div class="buttons" v-scroll-reveal.reset='{delay:250}'>
+      <md-button 
+      v-for="(button, index) in buttons" 
       :key="index" 
       :class="button.active ? 'md-raised' : ''"
       @click="changeMenu(button.name)">
-        {{ button.name}}
+        {{ button.name }}
       </md-button>
+    </div>
+
+    <div class="products_items" v-scroll-reveal.reset='{delay:500}'>
+      <md-card
+        v-for="(product, index) in products"
+        :key="index"
+        v-show="product.type === activeProduct">
+        <md-card-media md-ratio='16:9'>
+          <img :src="require(`../../assets/images/products/${product.img}`)">
+        </md-card-media>
+        <md-card-header>
+          <h2 class="md-title">
+            {{ product.name }}
+          </h2>
+          <div class="md-subhead">
+            <span>${{ product.price }}</span>
+          </div>
+        </md-card-header>
+        <md-card-content>
+          {{ product.desc }}
+        </md-card-content>
+      </md-card>
     </div>
   </div>
 </template>
@@ -20,22 +43,53 @@ export default {
     return {
       buttons: [
         { name: "pizza", active: true },
-        { name: "burger", active: false },
+        { name: "burgers", active: false },
         { name: "salad", active: false },
         { name: "dessert", active: false }
-      ]
+      ],
+      products: [],
+      activeProduct: "pizza"
     };
   },
-  mathods: {
+  methods: {
     changeMenu(value) {
       this.buttons.forEach(item => {
         if (item.name == value) {
           item.active = true;
+          this.activeProduct = value;
         } else {
           item.active = false;
         }
       });
     }
+  },
+  created() {
+    this.$http
+      .get("products.json")
+      .then(response => response.json())
+      .then(data => {
+        let list = [];
+        for (let key in data) {
+          list.push({
+            ...data[key],
+            id: key
+          });
+        }
+        this.products = list;
+      });
   }
 };
 </script>
+
+<style scoped>
+.md-card {
+  width: 32%;
+  margin-bottom: 20px;
+}
+
+.products_items {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { MdButton, MdDrawer, MdTabs } from "vue-material/dist/components";
+import VueResource from "vue-resource";
+import VueScrollReveal from "vue-scroll-reveal";
+import { MdButton, MdDrawer, MdCard } from "vue-material/dist/components";
 import "vue-material/dist/vue-material.min.css";
 
 Vue.config.productionTip = false;
 
 Vue.use(MdButton);
 Vue.use(MdDrawer);
-Vue.use(MdTabs);
+Vue.use(MdCard);
+Vue.use(VueResource);
+Vue.use(VueScrollReveal, {
+  duration: 800,
+  scale: 1,
+  distance: "50px"
+});
+
+Vue.http.options.root = "https://domnoo-2c169.firebaseio.com/";
 
 new Vue({
   render: h => h(App)
