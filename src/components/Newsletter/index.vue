add80c62cc13ebeb38de4b2a1821ca6a951324ee
Felix Nana Yaw Yeboah <info@lunarstudio.com>
2019-01-05 10:39:23 +0000

    Finishing Touches

--- src/components/Newsletter/index.vue
+++ src/components/Newsletter/index.vue
@@ -41,9 +41,30 @@ export default {
       let valid = this.validate(this.email);
       if (valid[0]) {
         this.error = "";
+        this.addEmail(this.email)
       } else {
         this.error = valid[1];
       }
+    },
+    addEmail(email) {
+      this.$http.get(`users.json?orderBy=\"email\"&&equalTo=\"${email}\"`)
+      .then(response => {
+        if(Object.getOwnPropertyNames(response.data).length === 0) {
+          this.$http.post(`users.json`, {email: this.email})
+          .then(response => {
+            this.success = 'Thank you'
+          })
+        } else {
+          this.success = "Already on the list"
+        }
+      })
+      this.clearSuccess()
+    },
+    clearSuccess() {
+      setTimeout(() => {
+        this.email = "",
+        this.success = ""
+      }, 3000)
     }
   }
 };
